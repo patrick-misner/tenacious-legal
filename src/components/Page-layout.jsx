e9cb09c7288f0ed1436587a1da2082f07600f74f
Patrick-Misner <misner.patrick@gmail.com>
2023-03-11 00:22:02 -0700

    more editing

--- src/components/Page-layout.jsx
+++ src/components/Page-layout.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Navbar from './nav/Navbar';
 
 const PageLayout = ({ children }) => (
-  <div className="page-layout">
+  <div className="page-layout bg-gray-100">
     <Navbar />
     <div className="page-layout__content">{children}</div>
   </div>
