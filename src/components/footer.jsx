579a90f09c8f5f6d4ff45699e7005d5373555901
Patrick-Misner <misner.patrick@gmail.com>
2023-03-23 01:27:08 -0600

    text size change

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => (
   <>
     <hr className="col-span-12 h-px my-2 bg-gray-200 border-0 dark:bg-gray-300" />
-    <div className="col-span-12 text-center md:text-left md:flex md:justify-between">
+    <div className="col-span-12 text-center md:text-left md:flex md:justify-between text-xs">
       <p>© 2009 Tenacious Legal Support. All rights reserved.</p>
       <div className="text-center pt-2 md:pt-0 md:text-left">
         Member of{' '}
