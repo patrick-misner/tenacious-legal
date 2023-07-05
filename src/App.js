ac04d8c099c32903988a09e7c1f0fbd1245ccc2c
Patrick-Misner <misner.patrick@gmail.com>
2023-07-04 19:37:57 -0600

    clear up page names

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import PageLayout from './components/PageLayout';
-import DashboardPage from './components/pages/DashBoardPage';
+import HomePage from './components/pages/HomePage';
 import FaqPage from './components/pages/FaqPage';
 import ContactPage from './components/pages/ContactPage';
 
@@ -9,7 +9,7 @@ const App = () => (
   <BrowserRouter>
     <PageLayout>
       <Routes>
-        <Route exact path="/" element={<DashboardPage />} />
+        <Route exact path="/" element={<HomePage />} />
         <Route path="/faq" element={<FaqPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
