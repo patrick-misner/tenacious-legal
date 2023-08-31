f6e46086d528e3fef538b120e07d0bf0b75dce4d
Patrick-Misner <misner.patrick@gmail.com>
2023-08-31 00:24:54 -0600

    google tag manager

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
+import { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import TagManager from 'react-gtm-module';
 import PageLayout from './components/PageLayout';
 import HomePage from './components/pages/HomePage';
 import FaqPage from './components/pages/FaqPage';
 import ContactPage from './components/pages/ContactPage';
 
-const App = () => (
-  <BrowserRouter>
-    <PageLayout>
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route path="/faq" element={<FaqPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Routes>
-    </PageLayout>
-  </BrowserRouter>
-);
+const tagManagerArgs = {
+  gtmId: 'AW-955538351',
+};
+TagManager.initialize(tagManagerArgs);
+
+const App = () => {
+  useEffect(() => {
+    // Pushing the 'pageview' event to the dataLayer when a route is accessed
+    window.dataLayer.push({
+      event: 'pageview',
+    });
+  }, []);
+  return (
+    <BrowserRouter>
+      <PageLayout>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/faq" element={<FaqPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </PageLayout>
+    </BrowserRouter>
+  );
+};
 
 export default App;
